6fe7538d075115e04f20eff21cc4b24f0f6d984e
gauquiebart <bart.gauquie@gmail.com>
2010-10-17 21:18:41 +0200

    iteration B1

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+  validates :title, :description, :price, :image_url, :presence => true
+  validates :title, :uniqueness => true
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  validates :image_url, :format => {
+          :with => %r{\.(gif|jpg|png)$}i,
+          :message => 'must be an URL for gif, jpg or png image.'
+  }
 end
